381be0be00c3ddf97eb45015256b1b1939917460
Evgeniy Lennik <eugeniy.lennik@gmail.com>
2023-03-09 15:37:46 +0300

    iter3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"github.com/eugeniylennik/alertics/internal/handlers"
+	"github.com/eugeniylennik/alertics/internal/router"
 	"github.com/eugeniylennik/alertics/internal/server"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"log"
 	"net/http"
 )
 
 func main() {
-	r := NewRouter()
+	r := router.NewRouter()
 	s := &http.Server{
 		Addr:    "localhost:8080",
 		Handler: r,
@@ -21,22 +19,4 @@ func main() {
 		}
 	}()
 	server.ShutdownServer(s)
-}
-
-func NewRouter() chi.Router {
-	r := chi.NewRouter()
-	h := handlers.NewStorage()
-	r.Use(middleware.DefaultLogger)
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Route("/update", func(r chi.Router) {
-		r.Post("/{type}/{name}/{value}", h.RecordMetrics)
-	})
-	r.Get("/", h.GetMetrics)
-	r.Route("/value", func(r chi.Router) {
-		r.Get("/{type}/{name}", h.GetSpecificMetric)
-	})
-	return r
 }
---
+++ internal/handlers/test/handler_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"github.com/eugeniylennik/alertics/internal/router"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_RecordMetrics(t *testing.T) {
+	r := router.NewRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	statusCode, body := testRequest(t, ts, "POST", "/update/gauge/Alloc/12.12")
+	assert.Equal(t, http.StatusOK, statusCode)
+	assert.Equal(t, body, "")
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (int, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	return resp.StatusCode, string(respBody)
+}
